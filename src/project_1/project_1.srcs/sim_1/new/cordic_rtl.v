4a8de003e3557009ad2b8772250ae2bf0642a99a
piktorwa <wpantak@student.agh.edu.pl>
2024-06-03 18:27:07 +0200

    drobne zmiany w cordic_rtl.v

--- src/project_1/project_1.srcs/sim_1/new/cordic_rtl.v
+++ src/project_1/project_1.srcs/sim_1/new/cordic_rtl.v
@@ -21,12 +21,13 @@ reg signed [W-1:0] sin;
 reg signed [W-1:0] x_base, n_x_2, x_tmp;
 reg [2:0] i;
 reg signed [W-1:0] i_table [0:4] = 
-{   12'b000010101010 * FXP_SCALE,
+{   
+    12'b000010101010 * FXP_SCALE,
     12'b000000110011 * FXP_SCALE,
     12'b000000011000 * FXP_SCALE,
     12'b000000001110 * FXP_SCALE,
-    12'b000000001001 * FXP_SCALE};
-
+    12'b000000001001 * FXP_SCALE
+};
 
 always @(posedge clock) begin
     if (reset == 1'b1) begin
