b326bd7b30dc10a1f945f2ac915fd44143a8f2fc
janek <janek12458@gmail.com>
2024-06-09 19:05:17 +0200

    created project for zynq axi mb #1: added rtl file

---
+++ src/project_1_axi_mb/ip_repo/taylor_ip_1.0/hdl/taylor_rtl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2024 19:04:07
+// Design Name: 
+// Module Name: taylor_rtl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module taylor_rtl(
+
+    );
+endmodule
