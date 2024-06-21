b29a1ceca90269384c7ea90ced17e062ae326e21
piktorwa <wpantak@student.agh.edu.pl>
2024-06-21 20:42:43 +0200

    Last changes

--- src/project_1_sim/project_1.sim/sim_1/behav/xsim/xsim.dir/taylor_sine_beh_behav/obj/xsim_1.c
+++ src/project_1_sim/project_1.sim/sim_1/behav/xsim/xsim.dir/taylor_sine_beh_behav/obj/xsim_1.c
@@ -3,14 +3,19 @@
 /*  /   /\/   /                                                       */
 /* /___/  \  /                                                        */
 /* \   \   \/                                                         */
-/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
 /*  /   /        All Right Reserved.                                  */
 /* /---/   /\                                                         */
 /* \   \  /  \                                                        */
 /*  \___\/\___\                                                       */
 /**********************************************************************/
 
-
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
 #include "iki.h"
 #include <string.h>
 #include <math.h>
@@ -25,14 +30,19 @@
 /*  /   /\/   /                                                       */
 /* /___/  \  /                                                        */
 /* \   \   \/                                                         */
-/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
 /*  /   /        All Right Reserved.                                  */
 /* /---/   /\                                                         */
 /* \   \  /  \                                                        */
 /*  \___\/\___\                                                       */
 /**********************************************************************/
 
-
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
 #include "iki.h"
 #include <string.h>
 #include <math.h>
@@ -44,32 +54,33 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-extern void execute_5(char*, char *);
-extern void execute_6(char*, char *);
-extern void execute_12(char*, char *);
-extern void execute_13(char*, char *);
-extern void execute_14(char*, char *);
-extern void execute_15(char*, char *);
-extern void execute_16(char*, char *);
-extern void execute_17(char*, char *);
-extern void execute_3(char*, char *);
-extern void execute_4(char*, char *);
-extern void execute_9(char*, char *);
-extern void execute_10(char*, char *);
-extern void execute_11(char*, char *);
-extern void execute_18(char*, char *);
-extern void execute_19(char*, char *);
-extern void execute_20(char*, char *);
-extern void execute_21(char*, char *);
-extern void execute_22(char*, char *);
-extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
-extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[20] = {(funcp)execute_5, (funcp)execute_6, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)execute_17, (funcp)execute_3, (funcp)execute_4, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_18, (funcp)execute_19, (funcp)execute_20, (funcp)execute_21, (funcp)execute_22, (funcp)transaction_4, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 20;
+IKI_DLLESPEC extern void execute_5(char*, char *);
+IKI_DLLESPEC extern void execute_6(char*, char *);
+IKI_DLLESPEC extern void execute_7(char*, char *);
+IKI_DLLESPEC extern void execute_13(char*, char *);
+IKI_DLLESPEC extern void execute_14(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_16(char*, char *);
+IKI_DLLESPEC extern void execute_17(char*, char *);
+IKI_DLLESPEC extern void execute_18(char*, char *);
+IKI_DLLESPEC extern void execute_3(char*, char *);
+IKI_DLLESPEC extern void execute_4(char*, char *);
+IKI_DLLESPEC extern void execute_10(char*, char *);
+IKI_DLLESPEC extern void execute_11(char*, char *);
+IKI_DLLESPEC extern void execute_12(char*, char *);
+IKI_DLLESPEC extern void execute_19(char*, char *);
+IKI_DLLESPEC extern void execute_20(char*, char *);
+IKI_DLLESPEC extern void execute_21(char*, char *);
+IKI_DLLESPEC extern void execute_22(char*, char *);
+IKI_DLLESPEC extern void execute_23(char*, char *);
+IKI_DLLESPEC extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[21] = {(funcp)execute_5, (funcp)execute_6, (funcp)execute_7, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)execute_17, (funcp)execute_18, (funcp)execute_3, (funcp)execute_4, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)execute_19, (funcp)execute_20, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)transaction_4, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 21;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/taylor_sine_beh_behav/xsim.reloc",  (void **)funcTab, 20);
+	iki_relocate(dp, "xsim.dir/taylor_sine_beh_behav/xsim.reloc",  (void **)funcTab, 21);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
@@ -97,17 +108,16 @@ void sensitize(char *);
 void simulate(char *);
 
 extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
-extern void implicit_HDL_SCinstatiate();
-
 extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
 extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_xsimdir_location_if_remapped(argc, argv)  ;
     iki_set_sv_type_file_path_name("xsim.dir/taylor_sine_beh_behav/xsim.svtype");
     iki_set_crvs_dump_file_path_name("xsim.dir/taylor_sine_beh_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/taylor_sine_beh_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    void* design_handle = iki_create_design("xsim.dir/taylor_sine_beh_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
